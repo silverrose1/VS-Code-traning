9541381b7e7270511709ce6aa6c26723c3755ab9
Ahmed Nassar <silverrose1@hotmail.com>
2024-12-06 15:12:19 +0200

    a

---
+++ python-learning/b.py
@@ -0,0 +1,20 @@
+a= True
+print ("hello" == "hell\u00f6")
+a= True
+print ("hello" == "hell\u00f6")
+a= True
+print ("hello" == "hell\u00f6")
+a= True
+print ("hello" == "hell\u00f6")
+a= True
+print ("hello" == "hell\u00f6")
+a= True
+print ("hello" == "hell\u00f6")
+a= True
+print ("hello" == "hell\u00f6")
+a= True
+print ("hello" == "hell\u00f6")
+a= True
+print ("hello" == "hell\u00f6")
+a= True
+print ("hello" == "hell\u00f6")
