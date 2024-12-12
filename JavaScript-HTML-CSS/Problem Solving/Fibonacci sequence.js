5aa0584bd019f74867c8b89e9abd385098ca7907
Ahmed Nassar <silverrose1@hotmail.com>
2024-12-13 00:06:53 +0200

    update

--- JavaScript-HTML-CSS/Problem Solving/Fibonacci sequence.js
+++ JavaScript-HTML-CSS/Problem Solving/Fibonacci sequence.js
@@ -1,11 +1,19 @@
 // https://www.netacad.com/content/jse1/1.0/courses/content/m5/en-US/assets/0e57f80072f33cf5dc1a8879fdd87b47e3c5e871.png
 
+// function fib(n){
+//     if (n <= 1){
+//         return n;
+//     }
+//     else{
+//         return fib(n-1) + fib(n-2);
+//     }
+// }
+// fib = (n) => n<=1 ? n : fib(n-1) + fib(n-2);
+
 function fib(n){
     if (n <= 1){
         return n;
     }
-    else{
-        return fib(n-1) + fib(n-2);
-    }
 }
-console.log(fib(6));
+
+console.log(fib(1));
--- JavaScript-HTML-CSS/Problem Solving/Timeout-interval.js
+++ JavaScript-HTML-CSS/Problem Solving/Timeout-interval.js
@@ -1,6 +1,6 @@
 let counter = 1;
 
-let intervalId = setInterval(function () {
+setInterval=(function () {
     console.log(counter++);
 }, 1000);
 
